000040a571ec0ef5fe63639f9a3fc3281d52cf9a
talzaken <talzaken@gmail.com>
2024-10-15 17:30:10 -0400

    Add script to create new experiment files

---
+++ experiments/generate_new_experiment.py
@@ -0,0 +1,28 @@
+import os
+from datetime import datetime
+import shutil
+
+if __name__ == "__main__":
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    new_directory_path = f'experiments/{now}'
+    os.mkdir(new_directory_path)
+
+
+    README_TEMPLATE = f"""
+
+    # EXPERIMENT {now}
+
+    # Training Data Notes
+
+    # Model Training Notes
+
+    # Application Notes
+
+
+    """
+
+    shutil.copytree('pointmouse', new_directory_path, dirs_exist_ok=True)
+        
+
+    with open(f"{new_directory_path}/README.md", "w") as f:
+        f.write(README_TEMPLATE)
